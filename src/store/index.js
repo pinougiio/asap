0f8a8062f50a39bd113aadd297a5c113c26a1035
Mourad Moudjeb <vadord78@gmail.com>
2024-05-14 13:33:07 +0100

    added axios and correctly configured vuex, changed the history page to new script setup (vue 3)

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,31 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
 
 export default createStore({
   state: {
+    agentList: {}
   },
   getters: {
   },
   mutations: {
+    GetAgentList(state, responseData) {
+      state.agentList =  responseData;
+    }
   },
   actions: {
+    GetAgentList({ commit }) {
+
+      axios.get('https://app.nocodb.com/api/v2/tables/mjn9iywd15ihr6k/records', 
+      {
+        headers: {
+          'accept': 'application/json',
+          'xc-token': 'xC-VsSp2FpSFOeIbB0-igVuj6urql2GueZozk-R3'
+        }
+      })
+      .then((response) => {
+        commit('GetAgentList', response.data);
+      });
+    }
   },
   modules: {
   }
--- src/views/HistoryView.vue
+++ src/views/HistoryView.vue
@@ -1,11 +1,12 @@
 <template>
     <h1>History</h1>
-    <VBtn color="primary" @click="fetchData">Update</VBtn>
+    <VBtn color="blue" @click="refresh()">Update</VBtn>
     <v-card class="mt-3" flat>
-      <VList v-if="agents"
-      lines="two">
+      <VList 
+        v-if="getData.list"
+        lines="two">
         <v-list-item 
-          v-for="(agent, index) in agents.list" 
+          v-for="(agent, index) in getData.list" 
           :key="index"
           :title="agent.name"
           :subtitle="'last connection : '+agent.last_con + ' Create_date : '+ agent.CreatedAt"
@@ -16,38 +17,20 @@
     </v-card>
 </template>
 
-<script>
-export default {
-  name: 'HistoryView',
-  props: {
-    name: String,
-    create_date: String,
-    last_con: String,
-  },
-  data() {
-    return{
-      agents: {},
-    }
-  },
-  methods: {
-    fetchData() {
-      fetch('https://app.nocodb.com/api/v2/tables/mjn9iywd15ihr6k/records', {
-        method: "GET",
-        headers: {
-          "accept": "application/json",
-          "xc-token": "xC-VsSp2FpSFOeIbB0-igVuj6urql2GueZozk-R3",
-        }
-      })
-        .then((response) => {
-          response.json().then((data) => {
-            this.agents = data;
-            console.log(data);
-          });
-        })
-        .catch((err) => {
-          console.error(err);
-        })
-    },
-  },
+<script setup>
+import store from '@/store';
+import { computed, onMounted } from 'vue';
+  
+const refresh =() => {
+  store.dispatch('GetAgentList')
 }
+
+const getData = computed(() => {
+  return store.state.agentList
+}) 
+
+onMounted(() => {
+  store.dispatch('GetAgentList')
+})  
+
 </script>
